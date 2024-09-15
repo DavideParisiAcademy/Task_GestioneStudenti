e12a13d8f0d2506df8b4f8148683773fd2dd1c87
DavideParisiAcademy <d.parisi@iemmeconsulting.it>
2024-09-16 01:06:06 +0200

    version 1.0

---
+++ Task_GestioneStudenti/Task_GestioneStudenti/Classes/Lista.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_GestioneStudenti.Classes
+{
+    internal class Lista
+    {
+        List<Studente> lista = new List<Studente>();
+        Studente stu = new Studente();
+
+        #region Metodi
+
+        //1) POST
+        public void AggiungiStudente(Studente stu)
+        {
+            try
+            {
+                lista.Add(stu);
+                Console.WriteLine("Inserimento avvenuto con successo");
+            } catch (Exception)
+
+            {
+                Console.WriteLine("ERRORE DI INSERIMENTO");
+            }
+        
+        }
+
+        //2) GET
+        public void StampaLista()
+        {
+            try
+            {
+                if (lista.Count > 0)
+                {
+                    foreach (Studente st in lista)
+                    {
+                        Console.WriteLine(st.ToString());
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Lista vuota");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex .Message);
+            }
+        }
+
+        //3) UPDATE
+        public void ModificaStudente()
+        {
+            try {
+
+                if (lista.Count == 0)
+                {
+                    Console.WriteLine("ERRORE: nessuno studente presente nella lista");
+                }
+                else
+                {
+                    Console.WriteLine("Inserisci la matricola dello studente");
+                    string? ricercaString = Console.ReadLine();
+                    int ricercaInt = Convert.ToInt32(ricercaString);
+                    foreach (Studente st in lista)
+                    {
+                        if (ricercaInt == st.GetMatricola())
+                        {
+                            //modifica completa dello studente
+
+                            Console.WriteLine("Modifica nome: ");
+                            st.Nome = Console.ReadLine();
+                            Console.WriteLine("Modifica Cognome: ");
+                            st.Cognome = Console.ReadLine();
+                            Console.WriteLine("Modifica voto: ");
+                            string? votoString = Console.ReadLine();
+                            st.Voto = Convert.ToInt32(votoString);
+                        }
+
+                        else
+                        {
+                            Console.WriteLine("Studente non trovato");
+
+                        }
+                    }
+                }
+             }           
+             catch(Exception ex)
+            {
+                Console.Write(ex .Message);
+            }
+        }
+
+        //4) DELETE
+        public void EliminaStudente()
+        {
+            try
+            {
+                Console.WriteLine("Inserire il nome dello studente da eliminare");
+                string? ricercaNome = Console.ReadLine();
+                if (ricercaNome is null)
+                {
+                    Console.WriteLine("ERRORE: il valore inserito non è valido");
+                }
+                else
+                {
+                    for (int i = 0; i < lista.Count; i++)
+                    {
+                        if (lista[i].Nome == ricercaNome)
+                        {
+                            lista.Remove(lista[i]);
+                            Console.WriteLine("Studente eliminato con successo");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Studente non trovato");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex .Message);
+            }
+        }  
+        
+        //5.1) VOTO MEDIO
+        public void VotoMedio()
+        {
+            try
+            {
+                double somma = 0;
+                if (lista.Count > 0)
+                {
+                    for (int i = 0; i < lista.Count; i++)
+                    {
+                        somma = somma + lista[i].Voto;
+                    }
+
+                    Console.WriteLine($"Voto medio: {somma / lista.Count()}");
+                }
+                else
+                {
+                    Console.WriteLine("ERRORE: la lista è vuota");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+        }
+
+        //5.2) VOTO MASSIMO
+        public void VotoMassimo()
+        {
+            try
+            {
+                double votoMax = lista[1].Voto;
+
+                if (lista.Count > 0)
+                {
+                    for (int i = 0; i < lista.Count; i++)
+                    {
+                        votoMax = Math.Max(lista[i].Voto, votoMax);
+                    }
+                    Console.WriteLine($"Voto massimo: {votoMax}");
+                }
+                else
+                {
+                    Console.WriteLine("ERRORE: la lista è vuota");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+        }
+        //5.3) VOTO MINIMO
+        public void VotoMinimo()
+        {
+            try
+            {
+                double votoMin = lista[1].Voto;
+                if (lista.Count > 0)
+                {
+                    for (int i = 0; i < lista.Count; i++)
+                    {
+                        votoMin = Math.Min(lista[i].Voto, votoMin);
+                    }
+                    Console.WriteLine($"Voto minimo: {votoMin}");
+                }
+                else
+                {
+                    Console.WriteLine("ERRORE: la lista è vuota");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }   
+        #endregion
+    }
+}
---
+++ Task_GestioneStudenti/Task_GestioneStudenti/Classes/Studente.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_GestioneStudenti.Classes
+{
+    internal class Studente
+    {
+        #region Attributi
+        public double Voto { get; set; }
+        public string? Nome { get; set; }
+        public string? Cognome { get; set; }
+
+        private long matricola;
+        Random rand = new Random();
+        List<long> matricole = new List<long>();
+        bool ciclo = true;
+
+        #endregion
+
+        public Studente() { }
+
+        #region Costruttore
+
+        //Costruttore con generazione randomica delle matricole
+        public Studente(string? varNome, string? varCognome, double varVoto)
+        {
+
+            this.Voto = varVoto;
+            this.Nome = varNome;
+            this.Cognome = varCognome;
+
+            //Generazione Matricola random non negativa
+
+             matricola = rand.NextInt64(0,10000);
+             matricola = (int)matricola;
+             matricole.Add(matricola);
+  
+            //Controllo di univocità della matricola (da rivedere: potenziale processo ricorsivo di verifica)
+
+            //for(int i = 0; i < matricole.Count; i++)
+            //{
+            //    if (matricole[i] == matricola)
+            //    {
+            //        matricola = rand.NextInt64();
+            //    }
+            //}
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return $"[Studente]:\nNome: {Nome}, Cognome: {Cognome}, Voto: {Voto}, Matricola: {matricola}";
+        }
+        public long GetMatricola()
+        {
+            return matricola;
+        }
+
+    }
+}
+
